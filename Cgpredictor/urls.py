bbebc90e8c564f7b62d9c28b1e0ebbc41a106f83
kunalsharma05 <ks05111996@gmail.com>
2016-02-05 03:52:40 +0530

    login done

--- Cgpredictor/urls.py
+++ Cgpredictor/urls.py
@@ -22,6 +22,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/', views.login, name='login'),
+    url(r'^login/', views.user_login, name='login'),
 
 ]
