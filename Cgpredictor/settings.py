f0fff75ed781f6239a7e4baa37189aa1602b3463
kunalsharma05 <ks05111996@gmail.com>
2016-02-05 02:59:49 +0530

    setting up stuff

--- Cgpredictor/settings.py
+++ Cgpredictor/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    
     }
 }
 
@@ -101,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
